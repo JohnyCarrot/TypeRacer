3e9d61f9a62d21df5e50ccbc0e2868da2c599afa
Roman Božik <web.kone@gmail.com>
2025-04-21 14:39:48 +0200

    mock fix

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,13 +1,13 @@
 import { http, HttpResponse } from 'msw'
 
 export const handlers = [
-    http.get('http://localhost:8000/texts/random/', () => {
+    http.get('*/texts/random/', () => {
         return HttpResponse.json({
             content: 'Creativity is not a single spark of brilliance but a small fire built over time. It must be fed with curiosity, patience, and bravery to try again. Eventually, that fire becomes a warm light guiding both you and others.'
         })
     }),
 
-    http.get('http://localhost:8000/bots/random/', () => {
+    http.get('*/bots/random/', () => {
         return HttpResponse.json({
             id: 17,
             name: 'Dizzy Dan',
